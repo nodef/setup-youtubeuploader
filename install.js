6e5c3eda37ad711cdd617ca947bff996bdb91321
Subhajit Sahu <wolfram77@gmail.com>
2018-11-17 20:40:54 +0530

    update: 19.2

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 
 // Global variables.
-const RELEASE = '19.1';
+const RELEASE = '19.2';
 const URLPREFIX = 'https://github.com/golangf/youtubeuploader/releases/download';
 const ARCH = {
   arm: 'armv7',
